81f1bc5467c6d315f2e6818014ee0326dba31f00
Elijah_Rhodes <mastergamer3dr@gmail.com>
2026-06-10 11:52:07 -0700

    Completed sortAndRemoveDuplicates() method

--- src/DuplicateRemover.java
+++ src/DuplicateRemover.java
@@ -1,4 +1,8 @@
+import java.util.ArrayList;
+//import java.util.HashSet;
+import java.util.TreeSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * The DuplicateRemover class provides a method to remove duplicate words
@@ -14,8 +18,18 @@ public class DuplicateRemover {
      * @return a sorted List<String> containing unique words from the input array
      */
     public static List<String> sortAndRemoveDuplicates(String[] words) {
-        // TODO
-        return null;
+        Set<String> s1 = new TreeSet<>();
+        List<String> l1 = new ArrayList<>();
+
+        for( String x : words )
+        {
+            s1.add(x);
+        }
+        for( String x : s1 )
+        {
+            l1.add(x);
+        }
+        return l1;
     }
 
     public static void main(String[] args) {
